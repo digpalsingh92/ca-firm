59d5e5e6b6f6e66d3fcf312d92b0aff48c2f2c57
Your Name <shimpysingh@Shimpys-MacBook-Air.local>
2024-08-01 00:55:53 +0530

    Basic webpage done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-slate-900 w-full h-[40%] text-white p-3">
+        <footer className="bg-slate-900 mt-5 w-full h-[40%] text-white p-3">
             <div className="w-full h-full text-center">
                 <p>&copy; 2024 CA kalpana & Associates. All Rights Reserved.</p>
                 <ul>
--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import user from "../Asset/user.png";
-import { LuWallet } from "react-icons/lu";
-
 export default function OurServices() {
   return (
     <>
@@ -10,66 +7,59 @@ export default function OurServices() {
           <div className="w-[90%] h-[450px] flex flex-col justify-center items-center">
             <div className="w-full h-[50px] flex items-center justify-center">
               <span className="text-3xl text-slate-800 font-bold">
-               Services we Offer
+                Services we Offer
               </span>
             </div>
             <div className="w-full h-[400px] gap-3 flex flex-row justify-center items-center">
-              <div className="w-[30%] h-[300px] box-border shadow-lg shadow-slate-400 flex justify-center hover:shadow-lg hover:shadow-slate-800">
-                <div className="w-[90%] h-[10] items-center flex gap-3 flex-col">
-                  <div className="w-full h-full justify-center items-center flex mt-3 flex-col gap-3">
-                      <span className="text-xl flex flex-col justify-center items-center font-bold text-slate-800">
-                        GST Filling
-                      </span>
-                      <div className=" flex flex-col justify-center items-center ">
-                        <p className="text-lg w-full font-light text-slate-800">
-                          Lorem ipsum, dolor sit amet consectetur adipisicing
-                          elit. Animi maxime praesentium perspiciatis
-                          perferendis quos obcaecati vitae, dolor ullam
-                          necessitatibus eos, ratione cum sint sunt facilis quas
-                          voluptates magni nobis rem.
-                        </p>
-                      </div>
-                  </div>
-                </div>
-              </div>
-              <div className="w-[30%] h-[300px] box-border shadow-lg shadow-slate-400 flex justify-center hover:shadow-lg hover:shadow-slate-800">
-                <div className="w-[90%] h-[10] items-center flex gap-3 flex-col">
-                  <div className="w-full h-full justify-center items-center flex mt-3 flex-col gap-3">
-                    <span className="text-xl flex flex-col justify-center items-center font-bold text-slate-800">
-                      Income Tax Returns
+              <div className="w-[30%] h-[280px] box-border shadow-lg shadow-slate-400 rounded-md flex justify-center items-center hover:shadow-lg hover:shadow-slate-300 hover:bg-teal-300 hover:text-white transition duration-300">
+                <div className="w-full flex gap-3 flex-col items-center">
+                  <div className="w-full h-full flex flex-col gap-3 justify-center items-center mt-3">
+                    <span className="text-xl font-bold text-slate-800 flex flex-col justify-center items-center">
+                      Goods & Service Tax (GST) Filing
                     </span>
-                    <div className=" flex flex-col justify-center items-center ">
-                      <p className="text-lg w-full font-light text-slate-800">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Animi maxime praesentium perspiciatis perferendis
-                        quos obcaecati vitae, dolor ullam necessitatibus eos,
-                        ratione cum sint sunt facilis quas voluptates magni
-                        nobis rem.
+                    <div className="flex flex-col justify-center items-center mx-5 hover:text-white">
+                      <p className="text-lg font-light text-start text-slate-800 ">
+                        The GST journey in India began in the year 2000, and
+                        since then, has come a long way in shaping...
                       </p>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="w-[30%] h-[300px] box-border shadow-lg shadow-slate-400 flex justify-center hover:shadow-lg hover:shadow-slate-800">
-                <div className="w-[90%] h-[10] items-center flex gap-3 flex-col">
-                  <div className="w-full h-full justify-center items-center flex mt-3 flex-col gap-3">
-                    <span className="text-xl flex flex-col justify-center items-center font-bold text-slate-800">
-                      GST Registration and GST Filings
+              <div className="w-[30%] h-[280px] box-border shadow-lg shadow-slate-400 rounded-md flex justify-center items-center hover:shadow-lg hover:shadow-slate-300 hover:bg-teal-300 hover:text-white transition duration-300">
+                <div className="w-full flex gap-3 flex-col items-center">
+                  <div className="w-full h-full flex flex-col gap-3 justify-center items-center mt-3">
+                    <span className="text-xl font-bold text-slate-800 flex flex-col justify-center items-center">
+                      Goods & Service Tax (GST) Filing
                     </span>
-                    <div className=" flex flex-col justify-center items-center ">
-                      <p className="text-lg w-full font-light text-slate-800">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Animi maxime praesentium perspiciatis perferendis
-                        quos obcaecati vitae, dolor ullam necessitatibus eos,
-                        ratione cum sint sunt facilis quas voluptates magni
-                        nobis rem.
+                    <div className="flex flex-col justify-center items-center mx-5 hover:text-white">
+                      <p className="text-lg font-light text-start text-slate-800 ">
+                        The GST journey in India began in the year 2000, and
+                        since then, has come a long way in shaping...
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="w-[30%] h-[280px] box-border shadow-lg shadow-slate-400 rounded-md flex justify-center items-center hover:shadow-lg hover:shadow-slate-300 hover:bg-teal-300 hover:text-white transition duration-300">
+                <div className="w-full flex gap-3 flex-col items-center">
+                  <div className="w-full h-full flex flex-col gap-3 justify-center items-center mt-3">
+                    <span className="text-xl font-bold text-slate-800 flex flex-col justify-center items-center">
+                      Goods & Service Tax (GST) Filing
+                    </span>
+                    <div className="flex flex-col justify-center items-center mx-5 hover:text-white">
+                      <p className="text-lg font-light text-start text-slate-800 ">
+                        The GST journey in India began in the year 2000, and
+                        since then, has come a long way in shaping...
                       </p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <button className="w-[10%] h-[50px] bg-teal-300 rounded-lg text-xl font-normal hover:bg-teal-400 hover:w-[11%] hover:font-semibold"> More Details</button>
+            <button className="w-[10%] h-[50px] bg-teal-300 rounded-lg text-xl font-normal hover:bg-teal-400 hover:w-[11%] hover:font-semibold">
+              More Details
+            </button>
           </div>
         </div>
       </section>
